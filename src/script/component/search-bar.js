eda171c92e5ac77afa19641bbbaadced84bfba18
Isham Fakhri <ishamfakhri01@gmail.com>
2020-09-12 08:46:56 +0700

    menambahkan component search-bar

---
+++ src/script/component/search-bar.js
@@ -0,0 +1,27 @@
+class SearchBar extends HTMLElement {
+
+    connectedcallback() {
+        this.render();
+    }
+
+    set clickEvent(event) {
+        this._clickeEvent = event;
+        this.render();
+    }
+
+    get value() {
+        return this.querySelector("#searchElement").value;
+    }
+
+    render() {
+        this.innerHTML = `
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" id="searchElement" type="search" placeholder="Cari data covid" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" id="searchButtonElement" type="submit">Cari</button>
+        </form>
+        `;
+        this.querySelector("#searchButtonElement").addEventListener("click", this._clickEvent);
+    }
+}
+
+customElements.define("search-bar", SearchBar);
